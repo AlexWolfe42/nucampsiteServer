c4de4284561e45b2db3e14df56d9d6f917ea7f94
AlexWolfe42 <57695284+AlexWolfe42@users.noreply.github.com>
2020-04-03 03:03:12 -0500

    Delete config.js

--- config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    'secretKey': '12345-67890-09876-54321',
-    'mongoUrl': 'mongodb://localhost:27017/nucampsite',
-    'facebook': {
-        clientId: '816514622176684',
-        clientSecret: 'afde3794a8be960591620cbcdb882081'
-    }
-}
