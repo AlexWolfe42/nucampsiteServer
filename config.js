16f9f628341de56d03c273efbe1f2e55b76e4ee4
Alex Wolfe <hughalexwolfe42@gmail.com>
2020-04-03 02:49:19 -0500

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl': 'mongodb://localhost:27017/nucampsite'
+    'mongoUrl': 'mongodb://localhost:27017/nucampsite',
+    'facebook': {
+        clientId: '816514622176684',
+        clientSecret: 'afde3794a8be960591620cbcdb882081'
+    }
 }
